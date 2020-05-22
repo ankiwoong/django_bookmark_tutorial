7580c91ae403b0da2adce4bcb8b47751cfe138bf
ankiwoong <ankiwoong@gmail.com>
2020-05-22 14:02:12 +0900

    북마크 수정 기능 구현

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('Site URL')
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
 
     def __str__(self):
         # 객체를 출력할 때 나타날 값
